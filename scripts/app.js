d6d5550e74258dbe836c93f9c5598382fc36993d
Lester Yiu <lester.ws.yiu@gmail.com>
2022-06-10 21:06:35 -0400

    added slide menu javascript

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,32 @@
-console.log('Connected!');
+const app = {};
+app.openMenuBtn = document.querySelector('#openMenu');
+app.closeMenuBtn = document.querySelector('#closeMenu');
+app.slideMenuEl = document.querySelector('.slideMenu');
+app.formEl = document.querySelector('form');
+app.submitBtn = document.querySelector('button[type="submit"]');
+
+app.slideMenuFunction = () => {
+    const slideMenuToggle = () => {
+        app.slideMenuEl.classList.toggle('hidden');
+    }
+
+    app.openMenuBtn.addEventListener('click', (e) => {
+        slideMenuToggle();
+    })
+
+    app.closeMenuBtn.addEventListener('click', () => {
+        slideMenuToggle();
+    })
+
+    app.slideMenuEl.addEventListener('click', (e) => {
+        if (e.target.localName === 'a') {
+            slideMenuToggle();
+        }
+    })
+}
+
+app.init = () => {
+    app.slideMenuFunction();
+}
+
+app.init();
